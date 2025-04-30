f68a28f3f232c4fc03b358a2c9c3ff1f31223418
HANSEO PARK <hapark@cern.ch>
2025-04-30 16:50:02 +0900

    update the scripts to avoid duplication

--- runPOWHEG.sh
+++ runPOWHEG.sh
@@ -10,6 +10,8 @@ echo "print POWHEG"
 #export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:/afs/cern.ch/work/h/hapark/work/PHD_Analysis/Run3/pp/13.6TeV/HfJets/PYTHIA/POWHEG/LHAPDF/lhapdf6/share/LHAPDF
 export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current
 
+### powheg parameter set on powheg.input
+sed -i '/^iseed/d' powheg.input
 echo "iseed $RANDOM" >> powheg.input
 
 pwhg_main_dijet >&pwhg.log
@@ -27,12 +29,31 @@ echo "Done load POWHEG and O2Physics"
 
 #export PYTHIA8DATA=$ALICE_ROOT/PYTHIA8/pythia8/xmldoc/
 
-root.exe -l -b -q 'RunPythia8.C++(100000)' > ana.log 2> ana.err
+### Set pythia
+NEV=100000
+root.exe -l -b -q 'RunPythia8.C++($NEV)' > ana.log 2> ana.err
 
 date
+BASE_RESDIR="pp13TeVdijet_CT18nlo"
+DATE_TAG=$(date +%y_%m_%d)
+RESDIR="$BASE_RESDIR"
+CHECK_PATH="/eos/user/h/hapark/PHD_Analysis/Run3/pp/13.6TeV/HfJets/PYTHIA/POWHEG/$RESDIR"
 
-WORKDIR="/eos/user/h/hapark/PHD_Analysis/Run3/pp/13.6TeV/HfJets/PYTHIA/POWHEG/pp13TeVdijet_CT18nlo/$2"
+if [ -d "$CHECK_PATH" ]; then
+    i=0
+    while true; do
+        RESDIR="${BASE_RESDIR}_${DATE_TAG}_$i"
+        CHECK_PATH="/eos/user/h/hapark/PHD_Analysis/Run3/pp/13.6TeV/HfJets/PYTHIA/POWHEG/$RESDIR"
+        if [ ! -d "$CHECK_PATH" ]; then
+            break
+        fi
+        ((i++))
+    done
+fi
+WORKDIR="/eos/user/h/hapark/PHD_Analysis/Run3/pp/13.6TeV/HfJets/PYTHIA/POWHEG/$RESDIR/$2"
+echo "work dir: $WORKDIR"
 
 mkdir -p $WORKDIR
+rm *.so *.pcm *.d
 
-rsync -av --exclude='_condor_stdout' --exclude='_condor_stderr' --exclude='tmp' --exclude='var' --exclude='condor_exec.exe' ./* $WORKDIR
+rsync -av --exclude="*/" --exclude='_condor_stdout' --exclude='_condor_stderr' --exclude='tmp' --exclude='var' --exclude='condor_exec.exe' ./* $WORKDIR
